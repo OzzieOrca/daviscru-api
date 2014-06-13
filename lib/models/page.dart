2f285ff4e68e0626930f4a6db3133b17237ffd3d
Scotty Waggoner <ozzieorca@gmail.com>
2014-06-12 22:02:12 -0700

    Implemented reordering of widgets. Addresses issue #4

--- lib/models/page.dart
+++ lib/models/page.dart
@@ -9,4 +9,22 @@ class Page {
   Page(this.title, this.url, this.parent, [this.widgets]);
 
   Page.fromJson(Map<String, dynamic> json) : this(json['title'], json['url'], json['parent'], json['widgets'].map((d) => new Widget.fromJson(d)).toList());
+
+  void moveWidgetUp(Widget currentWidget){
+    int index = widgets.indexOf(currentWidget);
+    if(index > 0 && index < widgets.length){
+      Widget temp = widgets[index];
+      widgets[index] = widgets[index - 1];
+      widgets[index - 1] = temp;
+    }
+  }
+
+  void moveWidgetDown(Widget currentWidget){
+    int index = widgets.indexOf(currentWidget);
+    if(index < widgets.length - 1){
+      Widget temp = widgets[index];
+      widgets[index] = widgets[index + 1];
+      widgets[index + 1] = temp;
+    }
+  }
 }
