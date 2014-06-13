fa6491e5b3fd77560461129450772891a037b5c5
Scotty Waggoner <ozzieorca@gmail.com>
2014-06-12 20:47:01 -0700

    Implemented all widgets that don't require multiple pieces (such as images). Addresses issue #4

--- lib/models/widget.dart
+++ lib/models/widget.dart
@@ -5,6 +5,8 @@ class Widget {
   String headingText = "Heading";
   String subHeadingText = "Subheading";
   String bodyText = "Paragraph Text";
+  String alignment = "left";
+  bool hasDivider = false;
   String imageUrl = "http://placehold.it/500x500/FFD34E/000000";
   String imageAlt;
   String videoUrl = "https://youtube.com/watch?v=2R_u1XPdJWA";
@@ -16,8 +18,13 @@ class Widget {
     headingText = json['headingText'];
     subHeadingText = json['subHeadingText'];
     bodyText = json['bodyText'];
+    alignment = json['alignment'];
+    hasDivider = json['hasDivider'];
     imageUrl = json['imageUrl'];
     imageAlt = json['imageAlt'];
     videoUrl = json['videoUrl'];
   }
+
+  void swapAlignment() => alignment == "left" ? alignment = "right" : alignment = "left";
+  void toggleDivider() => hasDivider = !hasDivider;
 }
