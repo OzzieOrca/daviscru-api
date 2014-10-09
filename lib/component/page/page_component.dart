f336d7459e3b643bf38bcccc0bc512e50c8306f1
Scotty Waggoner <ozzieorca@gmail.com>
2014-10-08 21:45:12 -0700

    Allow IFrame in HTML widget. Adjusted CSS margin for marketing containers (featurette)

--- lib/component/page/page_component.dart
+++ lib/component/page/page_component.dart
@@ -1,5 +1,6 @@
 library page;
 
+import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:daviscru/service/authentication.dart';
 import 'package:daviscru/service/global.dart';
@@ -12,8 +13,36 @@ part 'package:daviscru/models/widget.dart';
     selector: 'page',
     templateUrl: '/packages/daviscru/component/page/page.html',
     publishAs: 'pgComp',
+    module: PageComponent.module,
     useShadowDom: false)
 class PageComponent {
+  static void module(DirectiveBinder binder) =>
+      binder.bind(NodeValidator, toFactory: () {
+        final validator = new NodeValidatorBuilder()
+          ..allowImages()
+          ..allowElement('IFRAME', attributes: ['src', 'width', 'height', 'marginwidth', 'marginheight', 'frameborder'])
+          ..allowElement('A', attributes: ['href','onclick','target'])
+          ..allowElement('BUTTON', attributes: ['btn-radio', 'btn-checkbox', 'btn-checkbox-false', 'btn-checkbox-true', 'popover', 'popover-animation', 'popover-placement', 'popover-title', 'popover-trigger', 'tooltip'])
+          ..allowElement('DIV', attributes: ['class', 'collapse', 'style'])
+          ..allowElement('FORM', attributes: ['class','role'])
+          ..allowElement('I', attributes: ['class','style'])
+          ..allowElement('IMG', attributes: ['class','style','src'])
+          ..allowElement('H1', attributes: ['class','style'])
+          ..allowElement('H2', attributes: ['class','style'])
+          ..allowElement('H3', attributes: ['class','style'])
+          ..allowElement('P', attributes: ['class','style'])
+          ..allowElement('TABLE', attributes: ['class','style'])
+          ..allowElement('TBODY', attributes: ['class','style'])
+          ..allowElement('UL', attributes: ['class','style'])
+          ..allowElement('LI', attributes: ['class','style'] )
+          ..allowElement('TR', attributes: ['class','style'])
+          ..allowElement('TD', attributes: ['class','style','align','colspan'])
+          ..allowElement('BR', attributes: ['class','style'])
+          ..allowElement('B', attributes: ['class','style']);
+
+        return validator;
+      }, visibility: Visibility.CHILDREN);
+
   final PageRepository _repo;
   final Authentication auth;
   final Global global;
