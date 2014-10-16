eb23498899b10956f434b6f9249ee86d3c1646f6
Scotty Waggoner <ozzieorca@gmail.com>
2014-10-15 20:25:16 -0700

    Update Redstone deployment/builder script

---
+++ tool/grind.dart
@@ -0,0 +1,10 @@
+pub run grinder:grind allimport 'package:grinder/grinder.dart';
+import 'package:redstone/tasks.dart';
+
+main(List<String> args) {
+  task('build', Pub.build);
+  task('deploy_server', deployServer, ['build']);
+  task('all', null, ['build', 'deploy_server']);
+
+  startGrinder(args);
+}
